66bda20c723de1c53c3665000f72a0ac21f9fbbf
tnosugar <tnosugar@gmail.com>
2014-12-04 14:30:20 +0100

    Create en.js to al.js
    Localisation to Albanian. Please let me know if i've ommitted something

---
+++ source/js/Core/Language/locale/en.js to al.js
@@ -0,0 +1,37 @@
+/* Albanian LANGUAGE	 
+	================================================== */
+	if(typeof VMM != 'undefined') {
+	       VMM.Language = {
+	              lang: "sq",
+	              api: {
+	                     wikipedia: "sq"
+	              },
+	              date: {
+	                     month: ["Janar", "Shkurt", "Mars", "Prill", "Maj", "Qershor", "Korrik", "Gusht", "Shtator", "Tetor", "Nentor", "Dhjetor"],
+	                     month_abbr: ["Jan.", "Shk.", "Mar", "Pri", "Maj", "Qer", "Kor", "Gus.", "Shta.", "Tet.", "Nen.", "Dhj."],
+	                     day: ["E Diele","E Hene", "E Marte", "E Merkure", "E Enjte", "E Premte", "E Shtune"],
+	                     day_abbr: ["D.","H.", "M.", "M.", "E.", "P.", "SH."]
+	              },
+	              dateformats: {
+	                     year: "yyyy",
+	                     month_short: "mmm",
+	                     month: "mmmm yyyy",
+	                     full_short: "mmm d",
+	                     full: "mmmm d',' yyyy",
+	                     time_short: "h:MM:ss TT",
+	                     time_no_seconds_short: "h:MM TT",
+	                     time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+	                     full_long: "mmm d',' yyyy 'at' h:MM TT",
+	                     full_long_small_date: "h:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+	              },
+	              messages: {
+	                     loading_timeline: "Koha e ngarkimit... ",
+	                     return_to_title: "Kthehu te titulli",
+	                     expand_timeline: "Zgjero Koheshtritjen",
+	                     contract_timeline: "Ngushto koheshtrirjen",
+	                     wikipedia: "Nga Wikipedia, enciklopedia e lire",
+	                     loading_content: "Permbajtja duke u ngarkuar",
+	                     loading: "Duke u ngarkuar"
+	              }
+	       }
+	}
