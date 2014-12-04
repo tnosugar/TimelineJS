1145254c9d9b9b1d99c7bed9a72272c6bc7057a4
tnosugar <tnosugar@gmail.com>
2014-12-04 15:10:07 +0100

    Create en.js to mk.js
    Macedonian language file

---
+++ source/js/Core/Language/locale/en.js to mk.js
@@ -0,0 +1,37 @@
+/*
+Macedonian LANGUAGE 
+if(typeof VMM != 'undefined') {
+       VMM.Language = {
+              lang: "mk",
+              api: {
+                     wikipedia: "mk"
+              },
+              date: {
+                     month: [ „јануари“, „февруари“, „март“, „април“, „мај“,„јуни“, „јули“, „август“, „септември“, „октомври“, „ноември“, „декември“], 
+                     month_abbr: [„јан. “, „фев. “,  „март“, „април“, „мај“, „јуни“, „јули“, „авг. “, „септ. “, „окт. “, „ноем. “, „дек.“],
+                     day: [„понеделник“, „вторник“, „среда“, „четврток“, „петок“, „сабота“, „недела“],
+                     day_abbr: [„пон.“, „втор.“, „ср.“, „четв.“, „пет.“, „саб.“, „нед.“]
+              }, 
+              dateformats: {
+                     year: "yyyy",
+                     month_short: "mmm",
+                     month: "mmmm yyyy",
+                     full_short: "mmm d",
+                     full: "mmmm d',' yyyy",
+                     time_short: "h:MM:ss TT",
+                     time_no_seconds_short: "h:MM TT",
+                     time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+                     full_long: "mmm d',' yyyy 'at' h:MM TT",
+                     full_long_small_date: "h:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+              },
+              messages: {
+                     loading_timeline:„Вчитување на хронологијата ...“
+                     return_to_title: „Вратете се на насловот“,
+                     expand_timeline:„Проширена хронологија“,
+                     contract_timeline:„Кратка хронологија“
+                     wikipedia:„Од Википедија, слободна енциклопедија“, 
+                     loading_content:„Вчитување на содржина“,
+                     loading:„Вчитување“, 
+              }
+       }
+}
